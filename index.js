0230b4d910119ab175b40c1c13388d3f607984f3
George Kalpakas <kalpakas.g@gmail.com>
2019-04-14 05:38:20 +0300

    fix(Program): make minor UI changes in commit info popup (for consistency)

--- index.js
+++ index.js
@@ -1051,8 +1051,8 @@ javascript:/* eslint-disable-line no-unused-labels *//*
             Committed on: ${info.committerDate.toLocaleString()}
           </small>
         </p>
-        <p style="align-items: flex-start; display: flex; font-size: 1.25em;">
-          <b>${subject}</b>
+        <p style="align-items: center; display: flex; font-size: 1.25em;">
+          <b style="flex: auto;">${subject}</b>
           <span style="color: gray; margin-left: 30px;">@${info.sha.slice(0, 7)}</span>
         </p>
         <pre style="margin-top: 24px;">${body || '<i>No body.</i>'}</pre>
